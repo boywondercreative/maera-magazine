901a0566ad3178604277be3037c1d3e6c5af3d56
Aristeides Stathopoulos <aristath@gmail.com>
2015-02-05 21:29:46 +0200

    added 2 modules

--- includes/class-Maera_MG_Customizer.php
+++ includes/class-Maera_MG_Customizer.php
@@ -37,22 +37,72 @@ class Maera_MG_Customizer {
 
     function settings( $controls ) {
 
-        $controls[] = array(
-            'type'     => 'radio',
-            'setting'  => 'sidebar_width',
-            'label'    => __( 'Sidebar Width', 'maera_mg' ),
-            'section'  => 'layout',
-            'default'  => '336',
-            'priority' => 1,
-            'choices'  => array(
-                '300' => __( '300px', 'maera_mg' ),
-                '320' => __( '320px', 'maera_mg' ),
-                '336' => __( '336px', 'maera_mg' ),
-            ),
-        );
+		$controls[] = array(
+			'type'     => 'radio',
+			'setting'  => 'sidebar_width',
+			'label'    => __( 'Sidebar Width', 'maera_mg' ),
+			'section'  => 'layout',
+			'default'  => '336',
+			'priority' => 1,
+			'choices'  => array(
+				'300' => __( '300px', 'maera_mg' ),
+				'320' => __( '320px', 'maera_mg' ),
+				'336' => __( '336px', 'maera_mg' ),
+			),
+		);
+
+		$controls[] = array(
+			'type'     => 'radio',
+			'setting'  => 'fpm_top',
+			'label'    => __( 'Frontpage Module - Top', 'maera_mg' ),
+			'section'  => 'frontpage',
+			'default'  => 'grid',
+			'priority' => 1,
+			'choices'  => array(
+				'none'   => __( 'None', 'maera_mg' ),
+				'grid_5' => __( '5-post grid', 'maera_mg' ),
+				'single_big' => __( 'Single Post, BIG', 'maera_mg' ),
+			),
+		);
+
+		$controls[] = array(
+			'type'     => 'select',
+			'setting'  => 'fpm_top_cat',
+			'label'    => __( 'Frontpage Module - Top - Category', 'maera_mg' ),
+			'section'  => 'frontpage',
+			'default'  => 'all',
+			'priority' => 2,
+			'choices'  => $this->get_categories(),
+		);
 
         return $controls;
 
     }
+
+	public function get_categories() {
+
+		$cats = array();
+		$cats['all'] = __( 'All', 'maera_mg' );
+
+		$args = array(
+			'type'                     => 'post',
+			'orderby'                  => 'count',
+			'order'                    => 'ASC',
+			'hide_empty'               => 1,
+			'hierarchical'             => 0,
+			'number'                   => 20,
+			'taxonomy'                 => 'category',
+			'pad_counts'               => false
+		);
+
+		$categories = get_categories( $args );
+
+		foreach ( $categories as $category ) {
+			$cats[$category->term_id] = $category->name;
+		}
+
+		return $cats;
+
+	}
 
 }

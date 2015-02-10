d3c9cb78c49d02114e563724c016268e0a54fc42
Aristeides Stathopoulos <aristath@gmail.com>
2015-02-10 03:21:49 +0200

    Use a front_main widget area instead of a loop on the frontpage

--- includes/class-Maera_MG_Widget_Areas.php
+++ includes/class-Maera_MG_Widget_Areas.php
@@ -16,6 +16,14 @@ class Maera_MG_Widget_Areas {
 			'before_title'  => '', // No need for these here, we'll add them in the twig template file.
 			'after_title'   => '', // No need for these here, we'll add them in the twig template file.
 		) );
+		register_sidebar( array(
+			'name'          => 'Frontpage Main',
+			'id'            => 'front_main',
+			'before_widget' => '', // No need for these here, we'll add them in the twig template file.
+			'after_widget'  => '', // No need for these here, we'll add them in the twig template file.
+			'before_title'  => '', // No need for these here, we'll add them in the twig template file.
+			'after_title'   => '', // No need for these here, we'll add them in the twig template file.
+		) );
 
 	}
 
